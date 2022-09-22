7188c62482d5733923967656c1467be41fdc4aaf
makarichevalm <makarichevalm@yandex.ru>
2022-09-22 14:36:16 +0300

    add second lab

--- lab12/main.js
+++ lab12/main.js
@@ -1,5 +1,5 @@
-canvas.width = (window.innerWidth * 90) / 100;
-canvas.height = (window.innerHeight * 90) / 100;
+//canvas.width = (window.innerWidth * 80) / 100;
+//canvas.height = (window.innerHeight * 95) / 100;
 const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
 let ctx = document.getElementById("canvas").getContext("2d");
@@ -8,8 +8,8 @@ function getNewMatrix(part) {
     let newM = [];
     for (let i = 0; i < part.length; i++) {
         newM[i] = [];
-        newM[i][0] = centerX + part[i][0] * 10;
-        newM[i][1] = centerY - part[i][1] * 10;
+        newM[i][0] = Math.round(centerX + part[i][0] * 10);
+        newM[i][1] = Math.round(centerY - part[i][1] * 10);
         newM[i][2] = 1;
     }
     return newM;
@@ -17,6 +17,7 @@ function getNewMatrix(part) {
 class Cat {
     draw(part) {
         let arr = getNewMatrix(part);
+        console.log(arr)
         for (let i = 0; i < arr.length - 1; i++) {
             ctx.beginPath();
             ctx.strokeStyle = "black";
@@ -29,23 +30,23 @@ class Cat {
 }
 //----------------------------Матрицы перемещения-------------------------------
 let B = [
-    [1, 0, 0],
-    [0, 1, 0],
+    [1,   0, 0],
+    [0,   1, 0],
     [-10, 0, 1]
 ];
 let A = [
-    [1, 0, 0],
-    [0, 1, 0],
+    [1,  0, 0],
+    [0,  1, 0],
     [10, 0, 1]
 ];
 let C = [
-    [1, 0, 0],
-    [0, 1, 0],
+    [1, 0,  0],
+    [0, 1,  0],
     [0, 10, 1]
 ];
 let D = [
-    [1, 0, 0],
-    [0, 1, 0],
+    [1,  0,  0],
+    [0,  1,  0],
     [0, -10, 1]
 ];
 //------------------------------------------------------------------------------------
@@ -75,9 +76,42 @@ myCat.draw(mTail);
 myCat.draw(mBody);
 myCat.draw(mLFoot);
 myCat.draw(mRFoot);
-// ---------------------метод перемещения------------------------
+let k = 1;
+// ---------------------Работа с объектом------------------------
 document.addEventListener("keydown", function (event) {
     switch (event.key) {
+        case "+":
+            Scale(mLEar, k+0.2);
+            Scale(mREar, k+0.2);
+            Scale(mHead, k+0.2);
+            Scale(mLEye, k+0.2);
+            Scale(mREye, k+0.2);
+            Scale(mNose, k+0.2);
+            Scale(mTail, k+0.2);
+            Scale(mBody, k+0.2);
+            Scale(mLFoot, k+0.2);
+            Scale(mRFoot, k+0.2);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            for (let i = 0; i < catMatrix.length; i++) {
+                    myCat.draw(catMatrix[i]);
+            }
+        break;
+        case "-":
+            Scale(mLEar, k-0.2);
+            Scale(mREar, k-0.2);
+            Scale(mHead, k-0.2);
+            Scale(mLEye, k-0.2);
+            Scale(mREye, k-0.2);
+            Scale(mNose, k-0.2);
+            Scale(mTail, k-0.2);
+            Scale(mBody, k-0.2);
+            Scale(mLFoot, k-0.2);
+            Scale(mRFoot, k-0.2);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            for (let i = 0; i < catMatrix.length; i++) {
+                    myCat.draw(catMatrix[i]);
+            }
+            break;
         case "ArrowLeft":
             MultiplyMatrix(mLEar, B);
             MultiplyMatrix(mREar, B);
@@ -91,9 +125,7 @@ document.addEventListener("keydown", function (event) {
             MultiplyMatrix(mRFoot, B);
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             for (let i = 0; i < catMatrix.length; i++) {
-                for (let j = 0; j < catMatrix[i].length; j++) {
                     myCat.draw(catMatrix[i]);
-                }
             }
             console.log("Left pressed");
             break;
@@ -110,9 +142,7 @@ document.addEventListener("keydown", function (event) {
             MultiplyMatrix(mRFoot, A);
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             for (let i = 0; i < catMatrix.length; i++) {
-                for (let j = 0; j < catMatrix[i].length; j++) {
                     myCat.draw(catMatrix[i]);
-                }
             }
             console.log("Right pressed");
             break;
@@ -129,9 +159,7 @@ document.addEventListener("keydown", function (event) {
             MultiplyMatrix(mRFoot, C);
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             for (let i = 0; i < catMatrix.length; i++) {
-                for (let j = 0; j < catMatrix[i].length; j++) {
                     myCat.draw(catMatrix[i]);
-                }
             }
             console.log("Up pressed");
             break;
@@ -148,12 +176,55 @@ document.addEventListener("keydown", function (event) {
             MultiplyMatrix(mRFoot, D);
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             for (let i = 0; i < catMatrix.length; i++) {
-                for (let j = 0; j < catMatrix[i].length; j++) {
                     myCat.draw(catMatrix[i]);
-                }
             }
             console.log("Down pressed");
             break;
+        case "r":
+            RotateObj(mLEar, 0.01);
+            RotateObj(mREar, 0.01);
+            RotateObj(mHead, 0.01);
+            RotateObj(mLEye, 0.01);
+            RotateObj(mREye, 0.01);
+            RotateObj(mNose, 0.01);
+            RotateObj(mTail, 0.01);
+            RotateObj(mBody, 0.01);
+            RotateObj(mLFoot, 0.01);
+            RotateObj(mRFoot, 0.01);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            for (let i = 0; i < catMatrix.length; i++) {
+                myCat.draw(catMatrix[i]);
+        }
     }
 });
 //--------------------------------------------------------------------
+//-----------------------------Поворот-------------------------------
+function RotateObj(A,f){
+    let mtxRotate = [
+        [Math.cos(f), Math.sin(f), 0],
+        [-Math.sin(f),Math.cos(f), 0],
+        [0, 0,  1] 
+    ]
+    var rowsA = A.length,
+        rowsB = mtxRotate.length,
+        colsB = mtxRotate[0].length;
+        for (var k = 0; k < colsB; k++) {
+            for (var i = 0; i < rowsA; i++) {
+                var t = 0;
+                for (var j = 0; j < rowsB; j++) t += A[i][j] * mtxRotate[j][k];
+                A[i][k] = t;//Number(t.toFixed(1));
+            }
+        }
+        console.log("A ", A)
+        return A;
+}
+//-----------------------------------------------------------------
+//---------------------------Масштабирование-----------------------
+function Scale(A,k){
+    let scaleMatrix=[
+        [k, 0, 0],
+        [0, k, 0],
+        [0, 0, 1]
+    ]
+    MultiplyMatrix(A, scaleMatrix);
+}
